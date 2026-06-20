a1d0e155ac6c50e3e47e861a803909cfa44ccb9a
eshivani07 <eshivani07@gmail.com>
2026-06-20 15:01:14 +0530

    Added image upload endpoint

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {
-        "message": "NutriLens Backend Running"
-    }
+    return {"message": "NutriLens Backend Running"}
 
 @app.get("/test")
 def test():
+    return {"status": "success"}
+
+@app.post("/analyze")
+async def analyze(image: UploadFile = File(...)):
+
+    filename = image.filename
+
     return {
-        "status": "success"
+        "status": "success",
+        "filename": filename,
+        "health_score": 80,
+        "warnings": [
+            "High Sugar"
+        ],
+        "alternatives": [
+            "Low Sugar Option"
+        ]
     }
