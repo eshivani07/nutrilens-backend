852b69d9c5fb270b4c5f2a1f617a1ef9492a31d0
Shivani Elagam <eshivani07@gmail.com>
2026-06-22 20:04:50 +0530

    Changed Gemini model 2

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def analyze(file: UploadFile = File(...)):
         )
 
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.5-flash",
             contents=[
                 image,
                 """
